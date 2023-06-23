e08df3502e14f859c8accffff45a85b899db3f32
IpekUsul <118449645+IpekUsul@users.noreply.github.com>
2023-06-23 11:02:21 +0300

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -19,4 +19,19 @@ def verify(index):
     print("Target found at index: ",index)
   else:
     print("Target not found in list")
+    
+    def verify(index):
+  if index is not None:
+    print("Target found at index: ",index)
+  else:
+    print("Target not found in list")
+  
+ 
+numbers= [1,2,3,4,5,6,7,8,9,19]
+result= binary_search(numbers,12)
+verify(result)
+
+result= binary_search(numbers,6)
+verify(result)
+  
   
